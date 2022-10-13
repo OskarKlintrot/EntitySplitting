10fffc0a9f6689c5d2833a935f2d8981d45f0c47
Oskar Klintrot <oskar.klintrot@gmail.com>
2022-10-13 10:16:01 +0200

    Add project files.

---
+++ EntitySplitting/DatabaseScript.cs
@@ -0,0 +1,58 @@
+﻿namespace EntitySplitting;
+
+internal static class DatabaseScript
+{
+    public const string CreatePostTable = """
+        CREATE TABLE [dbo].[Posts](
+            [Id] [int] IDENTITY(1,1) NOT NULL,
+            [Title] [nvarchar](max) NOT NULL,
+            [Content] [nvarchar](max) NOT NULL,
+            CONSTRAINT [PK_Posts] PRIMARY KEY CLUSTERED 
+        (
+            [Id] ASC
+        )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+        ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+        """;
+
+    public const string CreateBlogTable = """
+        CREATE TABLE [dbo].[Blogs](
+            [Id] [int] IDENTITY(1,1) NOT NULL,
+            [Url] [nvarchar](max) NOT NULL,
+            CONSTRAINT [PK_Blogs] PRIMARY KEY CLUSTERED 
+        (
+            [Id] ASC
+        )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+        ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+        """;
+
+    public const string CreateJoinTable = """
+        CREATE TABLE [dbo].[BlogPost](
+            [BlogId] [int] NOT NULL,
+            [PostId] [int] NOT NULL,
+            CONSTRAINT [PK_BlogPost] PRIMARY KEY CLUSTERED 
+        (
+            [BlogId] ASC,
+            [PostId] ASC
+        )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+        ) ON [PRIMARY]
+        
+        CREATE UNIQUE NONCLUSTERED INDEX [IX_BlogPost_PostId] ON [dbo].[BlogPost]
+        (
+        	[PostId] ASC
+        )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+        """;
+
+    public const string AddConstraints = """
+        ALTER TABLE [dbo].[BlogPost]  WITH CHECK ADD  CONSTRAINT [FK_BlogPost_Blogs_BlogId] FOREIGN KEY([BlogId])
+        REFERENCES [dbo].[Blogs] ([Id])
+        ON DELETE CASCADE
+            
+        ALTER TABLE [dbo].[BlogPost] CHECK CONSTRAINT [FK_BlogPost_Blogs_BlogId]
+            
+        ALTER TABLE [dbo].[BlogPost]  WITH CHECK ADD  CONSTRAINT [FK_BlogPost_Posts_PostId] FOREIGN KEY([PostId])
+        REFERENCES [dbo].[Posts] ([Id])
+        ON DELETE CASCADE
+
+        ALTER TABLE [dbo].[BlogPost] CHECK CONSTRAINT [FK_BlogPost_Posts_PostId]
+        """;
+}
---
+++ EntitySplitting/Program.cs
@@ -0,0 +1,116 @@
+﻿using EntitySplitting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static EntitySplitting.DatabaseScript;
+
+using (var dbContext = new BloggingContext())
+{
+    await BloggingContext.InitializeAsync(dbContext);
+}
+
+using (var dbContext = new BloggingContext())
+{
+    dbContext.Blogs.Add(
+        new Blog
+        {
+            Url = "https://blog.google.com",
+            Posts =
+            {
+                new Post { Title = "Hello", Content = "world!" }
+            }
+        }
+    );
+
+    await dbContext.SaveChangesAsync();
+}
+
+using (var dbContext = new BloggingContext())
+{
+    var blog = await dbContext.Blogs.Include(x => x.Posts).SingleAsync();
+
+    Console.WriteLine($"Blog {blog.Url} contains {blog.Posts.Count} posts.");
+}
+
+namespace EntitySplitting
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=EntitySplitting;Trusted_Connection=True;MultipleActiveResultSets=true"
+            );
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BloggingContext).Assembly);
+        }
+
+        public static async Task InitializeAsync(BloggingContext context)
+        {
+            await context.Database.EnsureDeletedAsync();
+
+            await context.Database.MigrateAsync();
+
+            await context.Database.ExecuteSqlRawAsync(CreatePostTable);
+
+            await context.Database.ExecuteSqlRawAsync(CreateBlogTable);
+
+            await context.Database.ExecuteSqlRawAsync(CreateJoinTable);
+
+            await context.Database.ExecuteSqlRawAsync(AddConstraints);
+        }
+    }
+
+    public class Blog
+    {
+        public Blog()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string Url { get; set; } = null!;
+
+        public virtual ICollection<Post> Posts { get; set; }
+
+        internal sealed class Configuration : IEntityTypeConfiguration<Blog>
+        {
+            public void Configure(EntityTypeBuilder<Blog> builder)
+            {
+                builder
+                    .HasMany(d => d.Posts)
+                    .WithMany(p => p.Blogs)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "BlogPost",
+                        l => l.HasOne<Post>().WithMany().HasForeignKey("PostId"),
+                        r => r.HasOne<Blog>().WithMany().HasForeignKey("BlogId"),
+                        j =>
+                        {
+                            j.HasKey("BlogId", "PostId");
+
+                            j.ToTable("BlogPost");
+
+                            j.HasIndex(new[] { "PostId" }).IsUnique();
+                        }
+                    );
+            }
+        }
+    }
+
+    public class Post
+    {
+        public Post()
+        {
+            Blogs = new HashSet<Blog>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+
+        public virtual ICollection<Blog> Blogs { get; set; }
+    }
+}
